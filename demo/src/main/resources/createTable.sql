7c67a6ca80570ad2ef3830792a26a87ab23e3845
CShiru <1848252061@qq.com>
2020-12-01 19:55:54 +0800

    sql_update

--- demo/src/main/resources/createTable.sql
+++ demo/src/main/resources/createTable.sql
@@ -3,33 +3,62 @@ create table students(
                          name varchar(32),
                          idCard varchar(32),
                          ethAccount varchar(64),
-                         advance1 varchar(64),
-                         advance2 varchar(64),
-                         advance3 varchar(64),
+                         ethPassword varchar(64),
                          undergraduate1 varchar(64),
                          undergraduate2 varchar(64),
                          undergraduate3 varchar(64),
                          undergraduate4 varchar(64),
                          undergraduate5 varchar(64),
                          undergraduate6 varchar(64),
-                         password varchar(32),
-                         score integer default 0
+                         systemAccount varchar(64),
+                         systemPassword varchar(64),
+                         score integer default 0,
+                         universityAndMajor integer
+
 );
 
 create table university(
+
                            id serial primary key ,
+                           systemAccount varchar(64),
+                           systemPassword varchar(64),
                            name varchar(32),
                            idCode varchar(32),
                            ethAccount varchar(64),
-                           quota integer
+                           ethPassword varchar(64)
+
+
 );
+
+create table major(
+                      id serial primary key ,
+                      code integer,
+                      name varchar(64)
+
+
+);
+
+create table university_major(
+                                 id serial primary key ,
+                                 universityId integer,
+                                 majorId integer,
+                                 studentNum integer,
+                                 score integer
+
+
+
+);
+
 
 create table examInstitute(
                               id serial primary key,
-                              ethAccount varchar(64)
+                              systemAccount varchar(64),
+                              systemPassword varchar(64),
+                              ethAccount varchar(64),
+                              ethPassword varchar(64)
 )
 ;
 create table admitted(
-    student integer,
-    university integer
+                         student integer,
+                         university integer
 )
