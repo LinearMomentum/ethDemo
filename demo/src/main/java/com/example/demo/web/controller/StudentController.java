f23868293958d027a6b094f1f9c3519ab0ab65cd
Linear <1025427368@qq.com>
2020-12-01 21:27:23 +0800

    controller update

--- demo/src/main/java/com/example/demo/web/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/web/controller/StudentController.java
@@ -6,6 +6,7 @@ import com.example.demo.web.tables.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.NoSuchAlgorithmException;
@@ -20,16 +21,23 @@ public class StudentController {
     @GetMapping("/student/{ethaccount}")
     public Student getStudent(@PathVariable("ethaccount") String ethaccount){
         return studentMapper.getStudentByAccount(ethaccount);
+
     }
     @GetMapping("/student/insert")
-    public Student insertStudent(Student student) throws NoSuchAlgorithmException {
+    public void insertStudent(Student student) throws NoSuchAlgorithmException {
         String s=student.getPassword();
         student.setPassword(Encryption.encryptPassword(s));
-        return studentMapper.inserStudent(student);
+        studentMapper.inserStudent(student);
     }
     @GetMapping("/student/update")
-    public Student updatePassword(Student student){
-        return studentMapper.updatePassword(student);
+    public void updatePassword(Student student){
+        studentMapper.updatePassword(student);
+    }
+    @GetMapping("/student/login")
+    public boolean login(@RequestParam String idcard,@RequestParam String password) throws NoSuchAlgorithmException {
+        password=Encryption.encryptPassword(password);
+        System.out.println(studentMapper.login(idcard).getPassword().equals(password));
+        return studentMapper.login(idcard).getPassword().equals(password);
     }
 
 }
--- demo/src/main/java/com/example/demo/web/mapper/StudentMapper.java
+++ demo/src/main/java/com/example/demo/web/mapper/StudentMapper.java
@@ -2,15 +2,20 @@ package com.example.demo.web.mapper;
 import com.example.demo.web.tables.Student;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 @Mapper
 public interface StudentMapper {
-    @Select("select * from department where ethaccount=#{ethaccount}")
+    @Select("select * from students where ethaccount=#{ethaccount}")
     public Student getStudentByAccount(String ethaccount);
 
     @Options(useGeneratedKeys = true,keyProperty = "id")
     @Insert("insert into students(name,idcard,ethaccount,password,score) values (#{name},#{idcard},#{ethaccount},#{password},#{score})")
+    public void inserStudent(Student student);
 
-    public Student inserStudent(Student student);
     @Update("update students set passowrd=#{password} where id=#{id}")
-    public Student updatePassword(Student student);
+    public void updatePassword(Student student);
+
+    @Select("select * from students where idcard=#{idcard}")
+    public Student login(String idcard);
 }
